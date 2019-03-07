d55671a0182224c2a9954e6ba6bf399d315ea903
Balaganesh Damodaran <asleepysamurai@gmail.com>
2019-03-07 21:56:49 +0530

    Implemented: TodoItem converted to function component with replacement for getDerivedStateFromProps

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,40 +2,38 @@
  * TodoItem Component
  */
 
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 
-class TodoItem extends Component {
-    constructor(props) {
-        super(props);
+function TodoItem({
+    id,
+    text,
+    toggleItemCompleted,
+    completedItemIds
+}) {
+    const [completed, setCompleted] = useState(false);
 
-        this.state = {
-            completed: false
-        };
+    const todoItemIndexInCompletedItemIds = completedItemIds.indexOf(id);
+    const isCompleted = todoItemIndexInCompletedItemIds > -1;
+
+    if (isCompleted !== completed) {
+        setCompleted(isCompleted);
     }
 
-    toggleItemCompleted = () => {
-        this.props.toggleItemCompleted(this.props.id);
-    }
+    const onToggle = useCallback(() => {
+        toggleItemCompleted(id);
+    }, [toggleItemCompleted, id]);
 
-    static getDerivedStateFromProps(props, state) {
-        const todoItemIndexInCompletedItemIds = props.completedItemIds.indexOf(props.id);
-
-        return { completed: todoItemIndexInCompletedItemIds > -1 };
-    }
-
-    render() {
-        return (
-            <div
-                className="todo-item">
-                <input
-                    id={`completed-${this.props.id}`}
-                    type="checkbox"
-                    onChange={this.toggleItemCompleted}
-                    checked={this.state.completed} />
-                <label>{this.props.text}</label>
-            </div>
-        );
-    }
+    return (
+        <div
+            className="todo-item">
+            <input
+                id={`completed-${id}`}
+                type="checkbox"
+                onChange={onToggle}
+                checked={completed} />
+            <label>{text}</label>
+        </div>
+    );
 };
 
 export default TodoItem;
