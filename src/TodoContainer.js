7cc03f7815a6af132bb93b31e9a5a3a1c9ab2772
Balaganesh Damodaran <asleepysamurai@gmail.com>
2019-03-07 22:25:42 +0530

    Refactor: Extracted out reducer and initState to separate functions

--- src/TodoContainer.js
+++ src/TodoContainer.js
@@ -6,51 +6,57 @@ import React, { useReducer, useCallback, useEffect } from 'react';
 import TodoItem from './TodoItem';
 import TodoInput from './TodoInput';
 
-function Todo() {
-    function generateID() {
-        return Date.now().toString(36) + '-' + (Math.random() + 1).toString(36).substring(7);
-    };
+const generateID = () => {
+    return Date.now().toString(36) + '-' + (Math.random() + 1).toString(36).substring(7);
+};
 
-    const [state, dispatch] = useReducer((state, action) => {
-        if (action.type === 'toggleItemCompleted') {
-            const { todoItemId } = action;
-            const todoItemIndexInCompletedItemIds = state.completedItemIds.indexOf(todoItemId);
+const reducer = (state, action) => {
+    if (action.type === 'toggleItemCompleted') {
+        const { todoItemId } = action;
+        const todoItemIndexInCompletedItemIds = state.completedItemIds.indexOf(todoItemId);
 
-            const completedItemIds = todoItemIndexInCompletedItemIds === -1 ?
-                state.completedItemIds.concat([todoItemId]) :
-                ([
-                    ...state.completedItemIds.slice(0, todoItemIndexInCompletedItemIds),
-                    ...state.completedItemIds.slice(todoItemIndexInCompletedItemIds + 1)
-                ]);
+        const completedItemIds = todoItemIndexInCompletedItemIds === -1 ?
+            state.completedItemIds.concat([todoItemId]) :
+            ([
+                ...state.completedItemIds.slice(0, todoItemIndexInCompletedItemIds),
+                ...state.completedItemIds.slice(todoItemIndexInCompletedItemIds + 1)
+            ]);
 
-            return { ...state, completedItemIds };
-        }
+        return { ...state, completedItemIds };
+    }
 
-        if (action.type === 'addTodoItem') {
-            const newTodoItem = {
-                text: action.text,
-                id: generateID()
-            };
+    if (action.type === 'addTodoItem') {
+        const newTodoItem = {
+            text: action.text,
+            id: generateID()
+        };
 
-            const todoItems = state.todoItems.concat([newTodoItem]);
-            return { ...state, todoItems };
-        }
+        const todoItems = state.todoItems.concat([newTodoItem]);
+        return { ...state, todoItems };
+    }
 
+    return state;
+};
+
+const initialState = {
+    todoItems: [],
+    completedItemIds: []
+};
+
+const initState = (state) => {
+    let savedTodos = localStorage.getItem('todos');
+
+    try {
+        savedTodos = JSON.parse(savedTodos);
+        return Object.assign({}, state, savedTodos);
+    } catch (err) {
+        console.log('Saved todos non-existent or corrupt. Trashing saved todos.');
         return state;
-    }, {
-        todoItems: [],
-        completedItemIds: []
-    }, (state) => {
-        let savedTodos = localStorage.getItem('todos');
+    }
+};
 
-        try {
-            savedTodos = JSON.parse(savedTodos);
-            return Object.assign({}, state, savedTodos);
-        } catch (err) {
-            console.log('Saved todos non-existent or corrupt. Trashing saved todos.');
-            return state;
-        }
-    });
+function Todo() {
+    const [state, dispatch] = useReducer(reducer, initialState, initState);
 
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(state));
